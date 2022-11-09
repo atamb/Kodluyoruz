7a96f72aae069a891892aaf800a8ef0c6e6623bc
Ata Murat Bozkurt <94479946+atamb@users.noreply.github.com>
2022-11-10 00:06:42 +0300

    Add files via upload

---
+++ Loop1.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Loop1 {
+    public static void main(String[] args) {
+        int i, count=0, sum=0 ,a=1;
+        double avg;
+        Scanner scan= new Scanner(System.in);
+        i=scan.nextInt();
+        while(a<i){
+            if((a%3==0)||(a%4==0)){
+                sum+=a;
+                count++;
+
+            }
+           a++;
+        }
+        avg=(float)sum / (float) count;
+        System.out.println(sum);
+        System.out.println(count);
+        System.out.println(avg);
+    }
+}
